32e9d4885be128d091def2654fcdcd631e986644
andre calheiros <andrecj.dev@gmail.com>
2024-08-23 22:20:49 -0300

    adicionando elementos iniciais da pagina de login

--- consultas-ja/src/pages/Login.jsx
+++ consultas-ja/src/pages/Login.jsx
@@ -1,13 +1,23 @@
 import LoginButton from "../components/LoginButton";
+import React, { useState } from 'react';
+
 
 function Login() {
+  const [nomeUsuario, setNomeUsuario] = useState('');
+  const [senha, setSenha] = useState('');
 
   return (
     <div style={{ backgroundColor: 'blue'}}>
-          <h1>Login page</h1>
-          <LoginButton />
+        <input onChange={() => setNomeUsuario(event.target.value)} id="usuario" type="text" placeholder="Nome do usuário"  />
+        <input onChange={() => setSenha(event.target.value)} type="password" placeholder="Senha" maxLength="20"/>
+
+        <h3>{nomeUsuario}</h3>
+        <h3>{senha}</h3>
+
+        <button>Login</button>
           
     </div>
+    
   )
 }
 
