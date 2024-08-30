da83c5734e14708705b03828160bc9b0e8977a25
pllsg96 <pedroluizlsg@gmail.com>
2024-08-29 21:33:08 -0300

    fix: className in style

--- consultas-ja/src/App.jsx
+++ consultas-ja/src/App.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
 
   return (
-    <div class="flex justify-center flex-row p-40">
-      <div class="p-10 bg-slate-200 space-y-3">
+    <div className="flex justify-center flex-row p-40">
+      <div className="p-10 bg-slate-200 space-y-3">
         <h1>Sign In</h1>
       
         <BrowserRouter>
@@ -17,10 +17,10 @@ function App() {
        </BrowserRouter>
        </div>
 
-       <div class="bg-slate-400 w-200 flex flex-col justify-center space-y-5 p-10">
+       <div className="bg-slate-400 w-200 flex flex-col justify-center space-y-5 p-10">
         <h1 style={{color: 'white'}}>Bem vindo ao Consultas Já</h1>
         <h1 style={{color: 'white'}}>Ainda não é cadastrado?</h1>
-        <button class="bg-slate-600 border rounded-lg hover:bg-slate-500" style={{color: 'white'}}>Cadastre-se</button>
+        <button className="bg-slate-600 border rounded-lg hover:bg-slate-500" style={{color: 'white'}}>Cadastre-se</button>
        </div>
       
       
