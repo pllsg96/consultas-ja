7f2d4be1ab58c5035484066cc2509fb81bd52d18
pllsg96 <pedroluizlsg@gmail.com>
2024-10-08 21:42:11 -0300

    feat: insere dados para popular o buscar

---
+++ consultas-ja/src/data/mock.js
@@ -0,0 +1,45 @@
+const mockData = [
+  {
+    nome: 'Fulano da Silva',
+    cpf: '123.456.789-00',
+    rg: 'MG-12.345.678',
+    telefone: '(11) 99999-9999',
+    endereco: 'Rua Falsa 123, Bairro Imaginário, Cidade Exemplo',
+    imageProfile: 'https://example.com/profile1.jpg'
+  },
+  {
+    nome: 'Beltrano de Souza',
+    cpf: '987.654.321-00',
+    rg: 'SP-98.765.432',
+    telefone: '(21) 98888-8888',
+    endereco: 'Avenida dos Testes 456, Bairro Fictício, Cidade Teste',
+    imageProfile: 'https://example.com/profile2.jpg'
+  },
+  {
+    nome: 'Ciclano dos Santos',
+    cpf: '456.789.123-00',
+    rg: 'RJ-45.678.912',
+    telefone: '(31) 97777-7777',
+    endereco: 'Praça da Simulação 789, Bairro Exemplo, Cidade Fictícia',
+    imageProfile: 'https://example.com/profile3.jpg'
+  },
+  {
+    nome: 'Maria de Oliveira',
+    cpf: '321.654.987-00',
+    rg: 'RS-32.165.498',
+    telefone: '(41) 96666-6666',
+    endereco: 'Alameda Falsa 101, Bairro Inexistente, Cidade Exemplo',
+    imageProfile: 'https://example.com/profile4.jpg'
+  },
+  {
+    nome: 'João Pereira',
+    cpf: '654.321.987-00',
+    rg: 'PE-65.432.198',
+    telefone: '(51) 95555-5555',
+    endereco: 'Rua Teste 202, Bairro Fictício, Cidade Modelo',
+    imageProfile: 'https://example.com/profile5.jpg'
+  }
+];
+
+
+export default mockData;
