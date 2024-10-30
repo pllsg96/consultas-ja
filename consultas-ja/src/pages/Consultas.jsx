5eb098274d89a11a9bc8de1869cc3218e116764c
andre calheiros <andrecj.dev@gmail.com>
2024-10-29 21:29:58 -0300

    test: inserindo tabela hardcode para testar layout

--- consultas-ja/src/pages/Consultas.jsx
+++ consultas-ja/src/pages/Consultas.jsx
@@ -1,11 +1,49 @@
-import LoginButton from '../components/LoginButton'
 
-function Consultas({ username }) {
+
+function Consultas( ) {
   return (
-    <div className='bg-orange-500'>
-      <h1 className='text-white'>Consultas</h1>
-      <h2>{username}</h2>
-      <LoginButton />
+
+
+    <div className="flex flex-col p-5">
+      <div>
+      
+        <div className="mb-10">
+          <h1 className="text-xl text-center">Pesquise por um registro!</h1>
+        </div>
+        
+      <input 
+      type="text" 
+      className="w-100 rounded-xl pl-2 mr-3 mb-8"
+      placeholder="Buscar..."
+      />
+      <button
+      className="bg-gray-300 p-1 rounded-3xl"
+      >Pesquisar</button>
+      </div>
+
+      <div>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th>nome</th>
+              <th>cpf</th>
+              <th>telefone</th>
+              <th>outros dados</th>
+            </tr>
+          </thead>
+            
+            <tbody>
+              <tr>
+              <td>André</td>
+              <td>123.456</td>
+              <td>(82)99999-9999</td>
+              <td>outros dados</td>
+            </tr>
+            </tbody>
+            
+        </table>
+      </div>
+      
     </div>
   );
 }
